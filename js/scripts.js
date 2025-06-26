1c76ff1681dd117cfd2338685bbe17dce4901bb1
Radyslav Dmytrenko <coder.radik@gmail.com>
2025-06-26 15:38:56 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,105 @@
+//Sliders
+//Services slider
+$(document).ready(function(){
+  $('.services__slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/slider-arrow.svg" alt="slide-arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/slider-arrow.svg" alt="slide-arrow"></button>',
+  });
+});
+
+//Swiper slider
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  autoplay: {
+    delay: 4000,
+  },
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+//Functions
+//Fixed header when we start scrolling
+function fixedHeader() {
+  const header = document.querySelector('.header__navbar');
+
+  document.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 20) {
+      header.classList.add('fixed-top');
+    } else {
+      header.classList.remove('fixed-top');
+    }
+  })
+}
+
+//Counter
+function counter() {
+  document.addEventListener("DOMContentLoaded", function() {
+    // You can change this class to specify which elements are going to behave as counters.
+    var elements = document.querySelectorAll(".scroll-counter")
+
+    elements.forEach(function(item) {
+      // Add new attributes to the elements with the '.scroll-counter' HTML class
+      item.counterAlreadyFired = false
+      item.counterSpeed = item.getAttribute("data-counter-time") / 45
+      item.counterTarget = +item.innerText
+      item.counterCount = 0
+      item.counterStep = item.counterTarget / item.counterSpeed
+
+      item.updateCounter = function() {
+        item.counterCount = item.counterCount + item.counterStep
+        item.innerText = Math.ceil(item.counterCount)
+
+        if (item.counterCount < item.counterTarget) {
+          setTimeout(item.updateCounter, item.counterSpeed)
+        } else {
+          item.innerText = item.counterTarget
+        }
+      }
+    })
+
+    // Function to determine if an element is visible in the web page
+    var isElementVisible = function isElementVisible(el) {
+      var scroll = window.scrollY || window.pageYOffset
+      var boundsTop = el.getBoundingClientRect().top + scroll
+      var viewport = {
+        top: scroll,
+        bottom: scroll + window.innerHeight,
+      }
+      var bounds = {
+        top: boundsTop,
+        bottom: boundsTop + el.clientHeight,
+      }
+      return (
+        (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+        (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
+      )
+    }
+
+    // Funciton that will get fired uppon scrolling
+    var handleScroll = function handleScroll() {
+      elements.forEach(function(item, id) {
+        if (true === item.counterAlreadyFired) return
+        if (!isElementVisible(item)) return
+        item.updateCounter()
+        item.counterAlreadyFired = true
+      })
+    }
+
+    // Fire the function on scroll
+    window.addEventListener("scroll", handleScroll)
+  })
+}
+
+//Call functions
+fixedHeader();
+counter();
